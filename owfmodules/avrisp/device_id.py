df55551cf6f236546e82ea7dba264dada2a75930
ghecko <ghecko78@gmail.com>
2020-03-19 18:20:55 +0100

    retrieve device name and info

--- owfmodules/avrisp/device_id.py
+++ owfmodules/avrisp/device_id.py
@@ -12,6 +12,7 @@ import codecs
 from octowire.spi import SPI
 from octowire.gpio import GPIO
 from octowire_framework.module.AModule import AModule
+from owfmodules.avrisp.avrisp_devices import avr_device
 
 
 class DeviceID(AModule):
@@ -33,11 +34,36 @@ class DeviceID(AModule):
              "Description": "set SPI baudrate (1000000 = 1MHz) maximum = 50MHz", "Default": 1000000},
         ]
 
+    def check_signature(self, vendor_id, part_family, part_number):
+        if vendor_id == b"\x00" and part_family == b"\x01" and part_number == b"\x02":
+            self.logger.handle("Device Locked. both Lock bits have been set. This prevents the memory blocks from "
+                               "responding. To erase the Lock bits, it is necessary to perform a valid 'Chip Erase'.",
+                               self.logger.ERROR)
+            return False
+        elif vendor_id in [b"\x00", b"\xFF"]:
+            self.logger.handle("The device is locked or not ready", self.logger.ERROR)
+            return False
+        if part_family == b"\xFF" and part_number == b"\xFF":
+            self.logger.handle("Device Code Erased (or Target Missing)", self.logger.ERROR)
+            return False
+        return True
+
+    def get_device_info(self, signature):
+        for device_signature, device_info in avr_device.items():
+            if device_signature == signature:
+                self.logger.handle("Device name: {}".format(device_info["name"]))
+                self.logger.handle("Device flash size: {}".format(device_info["flash_size"]))
+                self.logger.handle("Device eeprom size: {}".format(device_info["eeeprom_size"]))
+                return device_info
+        else:
+            self.logger.handle("Device ID not found. Enable to identify the target device.")
+            return None
+
     def manage_resp(self, msg, resp):
         if resp is not None:
             self.logger.handle("{}: {}".format(msg, codecs.encode(resp, 'hex').decode().upper()), self.logger.RESULT)
         else:
-            self.logger.handle("Unable to get a response while reading the device response", self.logger.ERROR)
+            raise Exception("Unable to get a response while reading the device response")
 
     def process(self, spi_interface, reset):
         read_device_id_base_cmd = b'\x30\x00'
@@ -57,9 +83,15 @@ class DeviceID(AModule):
         part_number = spi_interface.receive(1)
         self.manage_resp(msg="Part Family and Flash Size", resp=part_number)
 
-        # Todo: print human readable part here (AT90S1200) and check for invalid response
-        # http://www.oocities.org/ve2olm/doc0943.pdf
+        # Drive reset high
+        reset.status = 1
 
+        # If the device signature is correct, return device information
+        if self.check_signature(vendor_id=vendor_id, part_family=part_family_and_size, part_number=part_number):
+            signature = codecs.encode(vendor_id + part_family_and_size + part_number, 'hex').decode()
+            return self.get_device_info(signature)
+        else:
+            return None
 
     def device_id(self):
         bus_id = self.get_option_value("spi_bus")
@@ -84,10 +116,7 @@ class DeviceID(AModule):
         spi_interface.transmit(enable_mem_access_cmd)
 
         self.logger.handle("Read device ID...", self.logger.INFO)
-        self.process(spi_interface, reset)
-
-        # Drive reset high
-        reset.status = 1
+        return self.process(spi_interface)
 
     def run(self, return_value=False):
         """
@@ -101,9 +130,9 @@ class DeviceID(AModule):
         if not self.owf_serial:
             return None
         try:
-            device_id = self.device_id()
+            device_info = self.device_id()
             if return_value:
-                return device_id
+                return device_info
             return None
         except (Exception, ValueError) as err:
             self.logger.handle(err, self.logger.ERROR)
