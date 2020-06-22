cf5c1d69f1ce90d2d5ae6c82520ed51a86f99c88
Jordan Ovrè <ghecko78@gmail.com>
2020-06-22 18:44:35 +0200

    Update headers

--- owfmodules/avrisp/device_id.py
+++ owfmodules/avrisp/device_id.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
 
-# Octowire SPI flash id module
-# Copyright (c) Jordan Ovrè / Paul Duncan
-# License: GPLv3
-# Paul Duncan / Eresse <eresse@dooba.io>
-# Jordan Ovrè / Ghecko <ghecko78@gmail.com
+# Octowire framework AVRISP device id module
+# Copyright (c) ImmunIT - Jordan Ovrè / Paul Duncan
+# License: Apache 2.0
+# Paul Duncan / Eresse <pduncan@immunit.ch>
+# Jordan Ovrè / Ghecko <jovre@immunit.ch>
 
 
 import codecs
