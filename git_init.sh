4c873283ad338934a84e9663677c7bde16105f28
ghecko <ghecko78@gmail.com>
2020-06-30 15:59:27 +0200

    Adding git_init script

---
+++ git_init.sh
@@ -0,0 +1,3 @@
+git remote set-url origin --push --add git@github.com:immunIT/owfmodules.avrisp.device_id.git
+git remote set-url origin --push --add git@bitbucket.org:octowire/owfmodules.avrisp.device_id.git
+git remote -v
